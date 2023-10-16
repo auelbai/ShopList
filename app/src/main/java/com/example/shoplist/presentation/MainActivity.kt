0a45073b9d5fdfc2d0f0f1952a7c8e7bd3b35409
auelbai <auelbai12@mail.ru>
2023-10-16 15:48:46 +0600

    add recyclerView Adapter, add recyclerView in MainActivity.kt

--- app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoplist.R
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var shopListAdapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +19,28 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
             Log.d("MainActivityTest", it.toString())
+        }
+
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        shopListAdapter = ShopListAdapter()
+        with(rvShopList) {
+            adapter = shopListAdapter
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.DISABLE_ITEM,
+                ShopListAdapter.RV_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.ENABLE_ITEM,
+                ShopListAdapter.RV_POOL_SIZE
+            )
+        }
+
+        shopListAdapter.onShopItemLongClickListener = {
+            viewModel.changeEnableState(it)
+        }
+
+        shopListAdapter.onShopItemClickListener = {
+            TODO()
         }
     }
 }
---
+++ app/src/main/java/com/example/shoplist/presentation/ShopListAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.shoplist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoplist.R
+import com.example.shoplist.domain.ShopItem
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+    var onShopItemLongClickListener: ((item: ShopItem) -> Unit)? = null
+    var onShopItemClickListener: ((item: ShopItem) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val layout = when (viewType) {
+            ENABLE_ITEM -> R.layout.item_shop_enabled
+            DISABLE_ITEM -> R.layout.item_shop_disabled
+            else -> throw RuntimeException("Unknown viewType")
+        }
+
+        val view = LayoutInflater.from(parent.context).inflate(
+            layout,
+            parent,
+            false
+        )
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        holder.tvText.text = shopItem.name
+        holder.tvCount.text = shopItem.count.toString()
+
+        holder.itemView.setOnLongClickListener {
+            onShopItemLongClickListener?.invoke(shopItem)
+            true
+        }
+
+        holder.itemView.setOnClickListener {
+            onShopItemClickListener?.invoke(shopItem)
+            true
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return shopList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val item = shopList[position]
+        return if (item.enabled) {
+            ENABLE_ITEM
+        } else DISABLE_ITEM
+    }
+
+    class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvText = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    companion object {
+        const val ENABLE_ITEM = 111
+        const val DISABLE_ITEM = 222
+        const val RV_POOL_SIZE = 10
+    }
+}
